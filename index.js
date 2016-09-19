ad83448d4402e9d09dc8780723bf0949220f8359
Aaron <aaronzimmermann92@gmail.com>
2016-09-19 14:34:49 +1000

    d

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function checkForQuit(p_message, p_session) {
 	var quitWords = ["don't", "worry", "quit", "stop", "nevermind"];
 	for(var i = 0; i < quitWords.length; i++) {
 		if(word == quitWords[i]) {
-			p_session.replaceDialog('/endCurrentDialog');
+			session.cancelDialog(0, '/endCurrentDialog');
 		}
 	}
 }
