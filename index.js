2ea54f3a5d91b298fb8c1375cc2a98babaf816ec
Aaron <aaronzimmermann92@gmail.com>
2016-10-01 15:05:02 +1000

    u

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
   }).resume();
-}).listen(process.env.PORT || 3000);
+}).listen(8080);
 
 
 //=========================================================
