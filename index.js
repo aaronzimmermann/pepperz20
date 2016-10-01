f3a4c01b29562651951e3821a4e5f26f288aecc5
Aaron <aaronzimmermann92@gmail.com>
2016-10-01 15:10:10 +1000

    u

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ var static = require('node-static');
 
 var file = new static.Server();
 require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
+  //request.addListener('end', function() {
     file.serve(request, response);
-  }).resume();
+  //}).resume();
 }).listen(8080);
 
 
