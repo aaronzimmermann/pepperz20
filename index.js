0c1b10f798c694467dc085ec28c88a2acf76b1f4
swapnilumehta <swapnilmehta@192-168-1-14.tpgi.com.au>
2016-10-12 09:24:00 +1100

    swap

--- index.js
+++ index.js
@@ -160,9 +160,14 @@ intents.matches('List', [
 }]);
 // Auto Repayment Date
 intents.matches('Auto Repayment Date', [
-    function (session, args, next) {
-		session.send("Due Date :");
-session.send(accountDate);
+    function (session, args, next) {var accountData = getAccount(p_accountName, p_session);
+	if(accountData != null) {
+		return accountData.Date;
+	} else {
+		return null;
+	}
+		
+//session.send(accountDate);
 }]);
 
 
