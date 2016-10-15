3c9c554151771acf70abeae7e51f7565e9f550e7
Aaron <aaronzimmermann92@gmail.com>
2016-10-16 00:15:27 +1100

    authentication

--- index.js
+++ index.js
@@ -271,6 +271,7 @@ function loginAsUser(p_newUserName, p_session) {
 	console.log("! " + customerData);
 	console.log("! " + customerData.customers);
 	console.log("! " + customerData.customers[0].name);
+	console.log("! " + customerData.customers[0].firstName);
 	for(var i = 0; i < customerData.customers.length; i++) {
 		if(customerData.customers[i].name.toLowerCase() == p_newUserName.toLowerCase()) {
 			p_session.userData.currentUser = p_newUserName.toLowerCase();
