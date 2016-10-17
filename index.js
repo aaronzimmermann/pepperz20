b41890262d7f70b63b6673fd1c57de1a75a45d10
swapnilumehta <swapnilmehta@192-168-1-10.tpgi.com.au>
2016-10-17 23:41:39 +1100

    swap

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ intents.matches('Repayment', [
 		session.send("Interest: " + accountInfo.interest + "\nBalance"+accountInfo.balance);
 	
 	} else {
-		session.send("Sorry the user doesnt have an account for auto, would you like to open one??? if you do then this would be helpful https://www.pepper.com.au");
+		session.send("Sorry the you don't have an account, would you like to open one??? if you do then this would be helpful https://www.pepper.com.au");
 	}
 		
 		
