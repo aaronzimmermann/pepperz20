84c4406cc0355325ca25c171410599a67adaeba1
swapnilumehta <swapnilmehta@192-168-1-10.tpgi.com.au>
2016-10-18 09:53:28 +1100

    swap

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ intents.matches('Repayment', [
 		// User states an account they do not have
 		else if(!checkValidAccountName(accountType.entity, session)) {
 			session.endDialog("I'm sorry but you don't have an " + accountType.entity + " account, if you want to open an account here is the link https://www.pepper.com.au ");
-			builder.Prompts.confirm(session, "Do you want to create a new account?");
+			//builder.Prompts.confirm(session, "Do you want to create a new account?");
 			
 		}
 		
@@ -229,14 +229,14 @@ intents.matches('Repayment', [
 		// Show a text summary
 		session.send("amount: " + accountInfo.amount + "\n\nInterest: " + accountInfo.interest + "\n\nBalance :"+accountInfo.balance);
 		}
-	function (session, results,next) {	
-		if(results.response == confirm_yes)
-		{
-		session.endDialog("I'm sorry but you don't have an " + accountType.entity + " account, if you want to open an account here is the link https://www.pepper.com.au ");
-	}else{
-	session.endDialog("no worries thanks for visiting");
-	}	
-	}
+	//function (session, results,next) {	
+	//	if(results.response == confirm_yes)
+	//	{
+	//	session.endDialog("I'm sorry but you don't have an " + accountType.entity + " account, if you want to open an account here is the link https://www.pepper.com.au ");
+	//}else{
+	//session.endDialog("no worries thanks for visiting");
+	//}	
+	//}
 ]);
 
 
