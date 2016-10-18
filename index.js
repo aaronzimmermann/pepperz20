007d56ff853e7118ccb991e9d648715d3d6290cc
swapnilumehta <swapnilmehta@192-168-1-10.tpgi.com.au>
2016-10-18 12:02:58 +1100

    swap

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ intents.matches('AccountDischarge', [
 //		}
 //	}
 //  ]);
-// REPAYMENT for any account (auto and mortgage)
+// Statement for any account (auto and mortgage)
 intents.matches('Repayment', [
     function (session, args, next) {
 		
@@ -241,14 +241,14 @@ intents.matches('Repayment', [
 		// User did not state an account
 		if(accountType == null) {
 			if(getNumAccounts(session) == 0) {
-				session.endDialog("I'm sorry but you don't have any accounts,if you want to open an account here is the link https://www.pepper.com.au");
+				session.endDialog("I'm sorry but you don't have any accounts.");
 			} else {
 				session.beginDialog('/getAccountName');
 			}
 		}
 		
 		else if(!checkValidAccountName(accountType.entity, session)) {
-			session.endDialog("I'm sorry but you don't have an " + accountType.entity + " account.if you want to open an account here is the link https://www.pepper.com.au");
+			session.endDialog("I'm sorry but you don't have an " + accountType.entity + " account.");
 		}
 		
 		
@@ -260,6 +260,9 @@ intents.matches('Repayment', [
 		
 		// Get account info
 		var accountInfo = getAccount(results.response, session);
+		
+		
+		//session.send("Here is a summary of your " + results.response + " statement: ");
 		session.send("Balance: " + accountInfo.balance + "\n\nInterest: " + accountInfo.interest + );
 		
 	}
